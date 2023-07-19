ca9a5182684344a2eaad3d9d4c7d42ad8ad5c780
jean-rosa <jeancarlosrosa22@gmail.com>
2023-07-19 11:35:07 -0300

    criando o jsx da tela de jogo

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,37 @@
 import '../styles/Game.css'
 
-export function Game({verifyLetter}){
-    return(
-        <div>
-            <h1>Game</h1>
-            <button onClick={verifyLetter}>Finalizar</button>
+export function Game({ verifyLetter }) {
+    return (
+        <div className='game'>
+            <p className='points'>
+                <span>Pontuação: 000</span>
+            </p>
+            <h1>Advinhe a Palavra</h1>
+            <h3 className='tipo'>
+                Dica Sobre a Palavra: <span>Dica...</span>
+            </h3>
+
+            <div className='wordContainer'>
+                <span className='letter'>A</span>
+                <span className='blankSquare'></span>
+            </div>
+
+            <div className='letterContainer'>
+                <p>Tente Advinhar uma letra:</p>
+                <form>
+                    <input type='text' name='letter' maxLength='1' required/>
+                    <button>Jogar!</button>
+                </form>
+            </div>
+
+            <div className='wrongLattersContainer'>
+                <p>Letras Já Utilizadas</p>
+                <span>a,</span>
+                <span>f,</span>
+                <span>h,</span>
+
+            </div>
+
         </div>
     )
 }
